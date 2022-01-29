f716632d7cfed8acc64f4e60db05ba0b42e579cc
nando <nando@hex7.com>
2022-01-29 10:43:19 -0500

    readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Temper Cloudwatch Integration
+
+This software provides a cronjob to publish temperature as CloudWatch metrics.
+
+It also also provides a bash script to set up CloudWatch alarms. These alarms notify whenever the temperature drops below a certain threshold.
+
+This code is a wrapper to Pham Urwen's temper software (https://github.com/urwen/temper).
--- temper.cloudwatch.alarms.sh
+++ temper.cloudwatch.alarms.sh
@@ -3,8 +3,28 @@
 
 
 # internal
-aws cloudwatch put-metric-alarm --alarm-name "temper-internal" --alarm-description "temper internal temperature low" --namespace "temper" --metric-name internal --statistic Minimum --period 300 --evaluation-periods 3 --threshold 20 --comparison-operator LessThanOrEqualToThreshold --dimensions Name=location,Value=sl
+aws cloudwatch put-metric-alarm \
+  --alarm-name "temper-internal" \
+  --alarm-description "temper internal temperature low" \
+  --namespace "temper" \
+  --metric-name internal \
+  --statistic Minimum \
+  --period 300 \
+  --evaluation-periods 3 \
+  --threshold 20 \
+  --comparison-operator LessThanOrEqualToThreshold \
+  --dimensions Name=location,Value=sl
 
 
 # external
-aws cloudwatch put-metric-alarm --alarm-name "temper-external" --alarm-description "temper external temperature low" --namespace "temper" --metric-name external --statistic Minimum --period 300 --evaluation-periods 3 --threshold 20 --comparison-operator LessThanOrEqualToThreshold --dimensions Name=location,Value=sl
+aws cloudwatch put-metric-alarm \
+  --alarm-name "temper-external" \
+  --alarm-description "temper external temperature low" \
+  --namespace "temper" \
+  --metric-name external \
+  --statistic Minimum \
+  --period 300 \
+  --evaluation-periods 3 \
+  --threshold 20 \
+  --comparison-operator LessThanOrEqualToThreshold \
+  --dimensions Name=location,Value=sl
